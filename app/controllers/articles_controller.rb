afb5ae54d8c22a119191727d585c43a97161d4df
Jon Lawrance <jonlawrance@jons-imac-5.home>
2019-12-18 18:25:30 +0000

    Add user data to articles listing page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,60 +1,60 @@
 class ArticlesController < ApplicationController
-before_action :set_article, only: [:edit, :update, :show, :destroy]
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
 
-def index
-@articles = Article.all
-end
+  def index
+    @articles = Article.all
+  end
 
-def new
-  @article = Article.new
-end
+  def new
+    @article = Article.new
+  end
 
-def edit
-
-end
-
-
-
-def create
-@article = Article.new(article_params)
-@article.user = User.first
-if @article.save
-  flash[:success] = "Article was successfully created"
-redirect_to article_path(@article)
-else
-  render 'new'
-end
-end
-
-def update
-
-if @article.update(article_params)
-  flash[:success] = "Article was successfully updated"
-  redirect_to article_path(@article)
-else
-  render 'edit'
-end
-end
+  def edit
+    
+  end
 
 
-def show
 
-end
+  def create
+    @article = Article.new(article_params)
+    @article.user = User.first
+    if @article.save
+      flash[:success] = "Article was successfully created"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+  end
 
-def destroy
+  def update
 
-@article.destroy
+    if @article.update(article_params)
+      flash[:success] = "Article was successfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
 
-redirect_to articles_path
-flash[:danger]="Article was successfully deleted"
-end
 
-private
-def set_article
-@article=Article.find(params[:id])
-end
-def article_params
-params.require(:article).permit(:title, :description)
-end
+  def show
+
+  end
+
+  def destroy
+
+    @article.destroy
+
+    redirect_to articles_path
+    flash[:danger]="Article was successfully deleted"
+  end
+
+  private
+  def set_article
+    @article=Article.find(params[:id])
+  end
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
 
 end
